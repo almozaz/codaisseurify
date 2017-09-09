bd9cc62a1afcc6228a545c32c775ae74b1d272ee
almozaz <alacruzmozaz@gmail.com>
2017-09-09 14:07:00 +0200

    Add Model tests: associations and validations

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  artist = Artist.create!( name: Faker::Lovecraft.deity , description: Faker::Lovecraft.fhtagn(3) )
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+  end
+
+  describe "association with songs" do
+    song1 = Song.create!(name: Faker::Lovecraft.tome, artist: artist )
+    song2 = Song.create!(name: Faker::Lovecraft.tome, artist: artist )
+
+    it "has many songs" do
+      expect(artist.songs).to include(song1)
+      expect(artist.songs).to include(song2)
+    end
+
+    it "deletes associated events" do
+    expect { artist.destroy }.to change(artist.songs, :count).by(-2)
+    end
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  artist  = Artist.create!( name: Faker::Lovecraft.deity , description: Faker::Lovecraft.fhtagn(3) )
+  song1   = Song.create!(name: Faker::Lovecraft.tome, artist: artist )
+  
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe "association with artist"do
+    it { is_expected.to belong_to :artist}
+  end
 end
