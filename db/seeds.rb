43b42377acf09979d1730c8b9ade1bb63f2d7a5b
almozaz <alacruzmozaz@gmail.com>
2017-09-10 12:24:03 +0200

    Create Photo: Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ artist1 = Artist.create!( name: Faker::Lovecraft.deity , description: Faker::Lov
 artist2 = Artist.create!( name: Faker::Lovecraft.deity , description: Faker::Lovecraft.fhtagn(3) )
 artist3 = Artist.create!( name: Faker::Lovecraft.deity , description: Faker::Lovecraft.fhtagn(3) )
 
+#artist1 songs
 5.times do
   Song.create!(
   name: Faker::Lovecraft.tome,
@@ -9,6 +10,7 @@ artist3 = Artist.create!( name: Faker::Lovecraft.deity , description: Faker::Lov
   )
 end
 
+#artist2 songs
 5.times do
   Song.create!(
   name: Faker::Lovecraft.tome,
@@ -16,9 +18,14 @@ end
   )
 end
 
+#artist3 songs
 5.times do
   Song.create!(
   name: Faker::Lovecraft.tome,
   artist: artist3
   )
 end
+
+photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dzc6qaitk/image/upload/v1504980241/maxresdefault_og3n9u.jpg", artist: artist1)
+photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dzc6qaitk/image/upload/v1504980241/artist2_rhjoga.jpg", artist: artist2)
+photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dzc6qaitk/image/upload/v1504980241/singer-career_ycq49m.jpg", artist: artist3)
