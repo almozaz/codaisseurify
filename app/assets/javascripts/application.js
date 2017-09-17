71560761d0c66a78388358c6ff7d2c68b3244886
almozaz <alacruzmozaz@gmail.com>
2017-09-17 18:17:53 +0200

    Add song implemented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,14 +7,12 @@
 
 
 var pathname = window.location.pathname;
-var parts = pathname.split("/");
-var artistId = parts[parts.length-1];
 
 function createSong(title) {
 
   $.ajax({
     type: "POST",
-    url:  "/api/artists/" + artistId + "/songs.json",
+    url:  "/api" + pathname + "/songs.json",
     data: JSON.stringify({
       name: title
     }),
@@ -23,55 +21,62 @@ function createSong(title) {
   })
   .done(function(data) {
   console.log(data)
+
   var deleteId = "song-" + data.song.id;
-  var listItem = $("<li></li>");
-  listItem.addClass("song");
-  listItem.attr('song-id', data.song.id);
+
+  var tableRow = $("<tr></tr>");
+  tableRow.addClass("song");
+  tableRow.attr('song-id', data.song.id);
+
+  var tableData = $("<td></td>");
+
   var deleteLink= $('<a></a>');
   deleteLink.attr('href', '#');
   deleteLink.attr('id', deleteId);
   deleteLink.attr('class', "delete-song");
   deleteLink.bind('click', deleteSong);
+
   var deleteIcon = $('<span></span>')
   deleteIcon.attr('class', "glyphicon glyphicon-remove");
 
-  var space = document.createTextNode(" ");
-
   var label = $('<label></label>');
   label.attr('for', deleteId);
-  label.html(data.title);
+  label.html(data.song.name);
 
   deleteLink.append(deleteIcon);
-  listItem.append(deleteLink);
-  listItem.append(space);
-  listItem.append(label);
+  
+  tableData.append(deleteLink);
+  tableData.append(label);
 
-  $("#songlist").append( listItem );
+  tableRow.append(tableData);
+
+  $("#song-list").find('tbody').append( tableRow );
+
   })
   .fail(function(error) {
     console.log(error);
-    // error_message = error.responseJSON.title[0];
+    // error_message = error.responseText;
     // showError(error_message);
   })
 }
 
-function showError(message) {
-  $("#songinput").addClass("error");
-  var errorElement = $("<small></small>")
-    .attr("id", "error_message")
-    .addClass("error")
-    .html(message);
-  $(errorElement).appendTo('form .field');
-}
+// function showError(message) {
+//   $("#songinput").addClass("error");
+//   var errorElement = $("<small></small>")
+//     .attr("id", "error_message")
+//     .addClass("error")
+//     .html(message);
+//   $(errorElement).appendTo('form .field');
+// }
 
-function resetErrors() {
-  $("#error_message").remove();
-  $("#songinput").removeClass("error");
-}
+// function resetErrors() {
+//   $("#error_message").remove();
+//   $("#songinput").removeClass("error");
+// }
 
 function submitSong(event) {
   event.preventDefault();
-  resetErrors();
+  // resetErrors();
   createSong($("#song_name").val());
   $("#song_name").val(null);
 }
@@ -82,7 +87,7 @@ function deleteSong(event) {
   var songId = $(listItem).data('id');
   $.ajax({
     type: "DELETE",
-    url: "/api/artists/" + artistId + "/songs/" + songId + ".json",
+    url: "/api" + pathname + "/songs/" + songId + ".json",
     contentType: "application/json",
     dataType: "json"})
   .done(function(data) {
